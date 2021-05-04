558e77be4c0f6a9aff50447b0cc04403ba4e1a35
oxine <ooxinee@gmail.com>
2021-05-04 22:20:59 +0800

    import video

---
+++ Truck/Assets/ImportVideo.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Diagnostics;
+using UnityEngine.UI;
+
+public class ImportVideo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Button Open;
+    // Use this for initialization
+    void Start()
+    {
+        //鼠标点击按钮事件
+        Open = GameObject.Find("Open").GetComponent<Button>();
+        Open.onClick.AddListener(OnClick);
+    }
+
+
+    public static bool ShowInExplorer(string itemPath)
+    {
+        bool result = false;
+
+#if !UNITY_WEBPLAYER
+        itemPath = Path.GetFullPath(itemPath.Replace(@"/", @"\"));
+        if (File.Exists(itemPath))
+        {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+
+            Process.Start("explorer.exe", "/select," + itemPath);
+#endif
+            result = true;
+        }
+        else if (Directory.Exists(itemPath))
+        {
+
+            UnityEngine.Application.OpenURL(itemPath);
+            result = true;
+        }
+
+#endif
+
+        return result;
+    }
+
+    public void OnClick()
+    {
+        ShowInExplorer("C:/");
+    }
+
+}
