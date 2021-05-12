96bc1a612b84405d31ae2dd822dc17ccf10b6505
oxine <ooxinee@gmail.com>
2021-05-12 10:07:49 +0800

    ScriptableObject Load&Save

---
+++ Truck/Assets/Scripts/AssetDataBase.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+
+public static class AssetDataBase
+{
+    public static void SaveAsset(string path, object obj)
+    {
+        try
+        {
+            if (File.Exists(Application.persistentDataPath + path))
+                Debug.LogWarning("save warning: " + "file already exists, override");
+            FileStream file = File.Create(Application.persistentDataPath + path);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(file, obj);
+            file.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("save error：" + e.Message);
+        }
+    }
+
+    public static T LoadAsset<T>(string path)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        object obj = null;
+        try
+        {
+            FileStream file = File.Open(Application.persistentDataPath + path, FileMode.Open, FileAccess.Read);
+            obj = formatter.Deserialize(file);
+            file.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("load error：" + e.Message);
+        }
+        return (T)obj;
+    }
+}
