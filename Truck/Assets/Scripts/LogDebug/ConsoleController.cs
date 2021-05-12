c0aeabfdc13b3874765a8f9f1cf3b587f19dade9
oxine <ooxinee@gmail.com>
2021-05-12 17:48:14 +0800

    Console change

--- Truck/Assets/Scripts/LogDebug/ConsoleController.cs
+++ Truck/Assets/Scripts/LogDebug/ConsoleController.cs
@@ -13,11 +13,13 @@ public class ConsoleController : Singleton<ConsoleController>
     [Tooltip("控制台输出框对象")]
     private Text outputText = null;
     Scrollbar scrollbar ;
+    RectTransform content;
 
     protected void Awake()
     {
         base.Awake();
         scrollbar = GetComponentInChildren<Scrollbar>();
+        content = outputText.transform.parent as RectTransform;
         outputText.text =
             "> Welcome To LiveCharacter2D \n";
         scrollbar.value = 1;
@@ -25,7 +27,20 @@ public class ConsoleController : Singleton<ConsoleController>
     
     public static void ShowMessage(object message)
     {
+        float textHeight = instance.outputText.preferredHeight;
         instance.outputText.text += "> " + message + "\n";
+        instance.outputText.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, textHeight);
+        instance.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, textHeight);
+        if (instance.content.parent.GetComponent<RectTransform>().rect.height < textHeight) {
+            print(instance.scrollbar.value);
+            LayoutRebuilder.ForceRebuildLayoutImmediate(instance.scrollbar.GetComponent<RectTransform>());
+            instance.StartCoroutine("Wait");
+            instance.scrollbar.value = 0;
+        }
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(1.0f);
         instance.scrollbar.value = 0;
     }
 
